5f40707c627299055dd242bc89962dbdba10477c
swarai2004 <135372731+swarai2004@users.noreply.github.com>
2024-08-19 00:03:02 -0400

    Full Crud APP
    CRUD APP PREMADE

---
+++ frontend/src/Update.js
@@ -0,0 +1,46 @@
+import axios from 'axios'
+import React, {useState} from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
+
+function Update() {
+    const [name, setName]= useState('')
+    const [phone, setPhone]= useState('')
+    const [email, setEmail]= useState('')
+    const navigate=useNavigate();
+    const {id}= useParams();
+    const handleSubmit=(event) => {
+      event.preventDefault();
+      axios.put('http://localhost:8081/update/'+id, {name, phone, email})
+      .then(res => {
+        console.log(res)
+        navigate('/');
+      }).catch(err => console.log(err));
+    }
+
+  return (
+    <div className='d-flex vh-100 bg-primary justify-content-center align-items-center'>
+      <div className='w-50 bg-white rounded p-3'>
+         <form onSubmit={handleSubmit}>
+              <h2>Update User</h2>
+              <div className='mb-2'>
+                  <label htmlFor="">Name</label>
+                  <input type="text" placeholder='Enter Name' className='form-control' onChange={e => setName(e.target.value)}/>
+
+              </div>
+              <div className='mb-2'>
+                   <label htmlFor="">Phone</label>
+                   <input type="text" placeholder='Enter Phone' className='form-control' onChange={e => setPhone(e.target.value)}/>
+              </div>
+              <div className='mb-2'>
+                   <label htmlFor="">Email</label>
+                   <input type="email" placeholder='Enter Email' className='form-control' onChange={e => setEmail(e.target.value)}/>
+              </div>
+              <button className='btn btn-success'>Update</button>
+            </form>
+        </div>
+    </div>
+
+  )
+}
+
+export default Update
